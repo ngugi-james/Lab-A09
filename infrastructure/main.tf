31f01e71e1b3d85699232ea0928a90eb040aaf46
ngugi-james <ngugijamesw@gmail.com>
2025-03-06 17:34:56 -0500

    Add resource creation Terraform script

---
+++ infrastructure/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  features {}
+}
+
+# Variables
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "myResourceGroup"
+}
+
+variable "location" {
+  description = "Azure region for the resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+  default     = "myVNet"
+}
+
+variable "address_space" {
+  description = "Address space for the Virtual Network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.address_space
+}
