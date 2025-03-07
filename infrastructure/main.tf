77e81597f2a046dd52d6f2f05b314122f8d313ea
ngugi-james <ngugijamesw@gmail.com>
2025-03-06 21:28:38 -0500

    Testing pre-commit hook

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -51,5 +51,4 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = var.address_space
 }
